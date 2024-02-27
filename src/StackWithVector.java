d0cb8f47da50519fb0bd31f6ca22b407326680e9
DerNait <75186178+DerNait@users.noreply.github.com>
2024-02-27 13:16:50 -0600

    Interfaz de Stack + Stack iniciales

---
+++ src/StackWithVector.java
@@ -0,0 +1,52 @@
+package src;
+
+import java.util.Vector;
+
+public class StackWithVector<T> implements IStack<T> {
+
+    private int index;
+    Vector<T> internalVector;
+
+    public StackWithVector(int qty){
+        index = 0;
+        internalVector = new Vector<T>(qty);
+    }
+
+    @Override
+    public int count() {
+        return internalVector.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return internalVector.isEmpty();
+    }
+
+    
+    @Override
+    public void push(T value) {
+        
+            internalVector.add(value);
+        
+    }
+
+    @Override
+    public T pop() {
+        T tempValue = null;
+        if (internalVector.size() > 0){
+            tempValue = internalVector.remove(internalVector.size() - 1);
+        }
+        
+        return tempValue;
+    }
+
+    @Override
+    public T peek() {
+        T tempValue = null;
+        if (internalVector.size() > 0){
+            tempValue = internalVector.get(internalVector.size() - 1);
+        }
+        return tempValue;
+    }
+    
+}
